265a797e75da1bd823e55617ad13eb0f5a65579c
Tatiane Abreu <108349796+TatianeAbreu@users.noreply.github.com>
2024-06-03 11:26:39 -0300

    If e Else

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ alert ("Boas vindas ao jogo do número secreto");
 let numeroSecreto = 38;
 let chute = prompt ("Escolha um número entre 1 e 40");
 
+// é para comentários - igual na robótica
+// if - se o chute for igual ao número secreto 
 if (numeroSecreto == chute) {
     console.log ("Isso mesmo! Você descobriu o número secreto (38)");
+}
+//
+else {
+    alert ("Você errou! :(");
 }
