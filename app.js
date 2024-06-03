834e888b80e20d15c7faf0157f564915d9f79d59
Tatiane Abreu <108349796+TatianeAbreu@users.noreply.github.com>
2024-06-03 11:50:39 -0300

    Concatenação - Live server

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 alert ("Boas vindas ao jogo do número secreto");
-let numeroSecreto = 38;
+let numeroSecreto = 9;
+console.log (numeroSecreto)
 let chute = prompt ("Escolha um número entre 1 e 40");
 
 // é para comentários - igual na robótica
 // if - se o chute for igual ao número secreto 
-if (numeroSecreto == chute) {
-    console.log ("Isso mesmo! Você descobriu o número secreto (38)");
+if (chute == numeroSecreto) {
+    alert (`Isso mesmo! Você descobriu o número secreto ${numeroSecreto}`);
 }
 //
 else {
