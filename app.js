5f877ed12b3476366ae608aa803bd5e1543d4c11
Tatiane Abreu <108349796+TatianeAbreu@users.noreply.github.com>
2024-06-03 11:21:08 -0300

    Iniciando - Comparando variáveis

---
+++ app.js
@@ -0,0 +1,7 @@
+alert ("Boas vindas ao jogo do número secreto");
+let numeroSecreto = 38;
+let chute = prompt ("Escolha um número entre 1 e 40");
+
+if (numeroSecreto == chute) {
+    console.log ("Isso mesmo! Você descobriu o número secreto (38)");
+}
